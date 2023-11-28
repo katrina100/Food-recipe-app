198a659fe8e3047f128f6518d6b5f18be2132b6a
katrina Alejo <katrinaalejo@katrinas-Air-2.ed.shawcable.net>
2023-11-28 01:15:08 -0700

    First commmit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Pages from "./pages/Pages.jsx";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Pages />
+    </div>
+  );
+}
+
+export default App;
